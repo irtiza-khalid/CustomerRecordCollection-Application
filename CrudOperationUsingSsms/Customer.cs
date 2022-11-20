a6e907a672a0f63ff11a4cee2f939ae3b0b3dffd
Irtiza khalid <100902172+irtiza-khalid@users.noreply.github.com>
2022-11-20 20:30:46 +0500

    Add project files.

---
+++ CrudOperationUsingSsms/Customer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.PeerToPeer.Collaboration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudOperationUsingSsms
+{
+    internal class Customer
+    {
+
+        public string id { get; set; }
+        public string companyname { get; set; }
+        public string contactname { get; set; }
+        public string phone { get; set; }
+
+
+        public string cusid;
+        public string companynam;
+        public string contactnam;
+        public string phon;
+        private static string myConn = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+
+
+        private const string InsertQuery = "insert Into customer(companyname, contactname, phone)  Values (@companyname, @contactname, @phone)";
+        public bool Insertcustomer(Customer customer)
+        {
+
+            int rows;
+            using (SqlConnection con = new SqlConnection(myConn))
+
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(InsertQuery, con))
+                {
+
+                    com.Parameters.AddWithValue("@companyname", Form1.companynmm);
+                    com.Parameters.AddWithValue("@contactname", Form1.contactnmm);
+                    com.Parameters.AddWithValue("@phone", Form1.pp);
+
+                    rows = com.ExecuteNonQuery();
+                }
+
+
+
+            }
+
+            return (rows > 0) ? true : false;
+        }
+
+
+        private const string SelectQuery = "Select* from customer";
+
+        public DataTable Getcustomer()
+
+        {
+            var datatable = new DataTable();
+            using (SqlConnection con = new SqlConnection(myConn))
+            {
+
+                con.Open();
+                using (SqlCommand com = new SqlCommand(SelectQuery, con))
+                using (SqlDataAdapter adapter = new SqlDataAdapter(com))
+                {
+                    adapter.Fill(datatable);
+
+
+                }
+
+                return datatable;
+
+            }
+
+
+
+
+        }
+        private const string updateQuery = "update customer set companyname=@companyname,contactname=@contactname,phone=@phone where ID_column=@ID_column ";
+        public bool updatecustomer(Customer customer)
+        {
+
+            int rows;
+            using (SqlConnection con = new SqlConnection(myConn))
+
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(updateQuery, con))
+                {
+
+                    com.Parameters.AddWithValue("@companyname", customer.companynam);
+                    com.Parameters.AddWithValue("@contactname",customer.contactnam );
+                    com.Parameters.AddWithValue("@phone",customer.phon);
+                    com.Parameters.AddWithValue("@ID_column",customer.cusid );
+                    rows = com.ExecuteNonQuery();
+                }
+
+
+
+            }
+
+            return (rows > 0) ? true : false;
+        }
+
+
+
+
+        private const string DeleteQuery = "Delete from customer where ID_column = @ID_column";
+
+        public bool deletecustomer(Customer customer)
+        {
+
+            int rows;
+            using (SqlConnection con = new SqlConnection(myConn))
+
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(DeleteQuery, con))
+                {
+
+                    
+                    com.Parameters.AddWithValue("@ID_column", customer.cusid);
+                    rows = com.ExecuteNonQuery();
+                }
+
+
+
+            }
+
+            return (rows > 0) ? true : false;
+        }
+
+    }
+}
---
+++ CrudOperationUsingSsms/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CrudOperationUsingSsms
+{
+   
+    public partial class Form1 : Form
+    {
+      
+
+        Customer customer=new Customer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static String pp;
+        public static String contactnmm;
+        public static String companynmm;
+        public static String id;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           pp=textBox3.Text;
+            contactnmm=textBox2.Text;
+            companynmm=textBox1.Text;
+            id = textBox4.Text;
+            customer.Insertcustomer(customer);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = customer.Getcustomer();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            customer.companynam = textBox1.Text;
+            customer.contactnam = textBox2.Text;
+            customer.phon = textBox3.Text;
+            customer.cusid = textBox4.Text;
+            var success = customer.updatecustomer(customer);
+            dataGridView1.DataSource = customer.Getcustomer();
+            if (success)
+
+            {
+                
+              
+                MessageBox.Show("Employee has been added successfully");
+            
+             }
+
+            else
+            {
+                MessageBox.Show("Error occured. Please try again...");
+            }
+ 
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            var index = e.RowIndex;
+
+            textBox1.Text = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[index].Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[index].Cells[0].Value.ToString();
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            customer.cusid = textBox4.Text;
+            var success = customer.deletecustomer(customer);
+            dataGridView1.DataSource = customer.Getcustomer();
+            if (success)
+
+            {
+
+
+                MessageBox.Show("Employee has been added successfully");
+
+            }
+
+            else
+            {
+                MessageBox.Show("Error occured. Please try again...");
+            }
+        }
+    }
+}
